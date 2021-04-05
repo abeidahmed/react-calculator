bf8167d45fc13f0d53b8489e9384a30d42736309
abeidahmed <abeidahmed92@gmail.com>
2021-04-05 14:50:20 +0530

    Add operate.js logic

---
+++ src/logic/operate.js
@@ -0,0 +1,34 @@
+import Big from 'big.js';
+
+const operate = (num1, num2, operation) => {
+  let result = new Big(0);
+  const one = new Big(num1);
+  let two;
+  let temp = false;
+
+  if (num2) two = new Big(num2);
+
+  switch (operation) {
+    case '+':
+      result = one.plus(two);
+      break;
+    case '-':
+      result = one.minus(two);
+      break;
+    case 'X':
+      result = one.times(two);
+      break;
+    case '÷':
+      if (num1 === '0' || num2 === '0') temp = true;
+      result = temp ? 'number cannot be divided by 0' : one.div(two);
+      break;
+    case '%':
+      result = one.times('0.01');
+      break;
+    default:
+  }
+
+  return result.toString();
+};
+
+export default operate;

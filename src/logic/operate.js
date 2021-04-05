18125476e2d8d3e5c6ee1435cdecf4f1e24d3a0b
abeidahmed <abeidahmed92@gmail.com>
2021-04-05 14:54:05 +0530

    Change variable names

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,28 +2,30 @@ import Big from 'big.js';
 
 const operate = (num1, num2, operation) => {
   let result = new Big(0);
-  const one = new Big(num1);
-  let two;
+  const numberOne = new Big(num1);
+  let numberTwo;
   let temp = false;
 
-  if (num2) two = new Big(num2);
+  if (num2) numberTwo = new Big(num2);
 
   switch (operation) {
     case '+':
-      result = one.plus(two);
+      result = numberOne.plus(numberTwo);
       break;
     case '-':
-      result = one.minus(two);
+      result = numberOne.minus(numberTwo);
       break;
     case 'X':
-      result = one.times(two);
+      result = numberOne.times(numberTwo);
       break;
     case '÷':
       if (num1 === '0' || num2 === '0') temp = true;
-      result = temp ? 'number cannot be divided by 0' : one.div(two);
+      result = temp
+        ? 'number cannot be divided by 0'
+        : numberOne.div(numberTwo);
       break;
     case '%':
-      result = one.times('0.01');
+      result = numberOne.times('0.01');
       break;
     default:
   }

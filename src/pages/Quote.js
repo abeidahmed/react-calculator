c6fa8b99d5dda871db6c496067384afd143a42d5
abeidahmed <abeidahmed92@gmail.com>
2021-04-08 14:22:52 +0530

    Create quote page

---
+++ src/pages/Quote.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Quote = () => <h1>hello from quote</h1>;
+
+export default Quote;
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Calculator from './pages/Calculator';
+import Quote from './pages/Quote';
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/calculator" component={Calculator} />
+      <Route path="/quote" component={Quote} />
     </Switch>
   </Router>
 );

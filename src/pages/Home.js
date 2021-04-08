f5a7d3f1314be3db66043c1facc8292f8a89dad8
abeidahmed <abeidahmed92@gmail.com>
2021-04-08 14:33:36 +0530

    Style home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Home = () => <h1>hello world</h1>;
+const Main = styled.main`
+  padding: 24px;
+`;
+
+const Home = () => (
+  <Main>
+    <h2>Welcome to our page!</h2>
+    <section>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </section>
+  </Main>
+);
 
 export default Home;
